e78fb0229c6d8c86130914d8e89d5e29e0421f85
denis <denis.beresnev@is.ru>
2025-10-27 11:30:13 +0300

    refactor: added verifier to handle logic

--- internal/server/router.go
+++ internal/server/router.go
@@ -1,11 +1,15 @@
 package server
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/shanth1/gitrelay/internal/verifier"
+)
 
 func (s *server) routes() *http.ServeMux {
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("POST /webhook", s.handleWebhook)
+	mux.HandleFunc("POST /webhook", s.handleGithubWebhook(&verifier.GithubVerifier{Secret: s.cfg.WebhookSecret}))
 	mux.HandleFunc("GET /health", s.handleHealthCheck)
 
 	mux.HandleFunc("/", s.handleRoot)
