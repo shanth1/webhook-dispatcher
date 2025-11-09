9e82055798907c87b0c737aeb8b0bc9f0d62578e
Denis Beresnev <shanthi.bunch@gmail.com>
2025-11-09 17:16:37 +0300

    refactor: github handler

--- internal/adapters/inbound/github/handler.go
+++ internal/adapters/inbound/github/handler.go
@@ -3,10 +3,8 @@ package github
 import (
 	"bytes"
 	"context"
-	"encoding/json"
 	"fmt"
 	"html/template"
-	"net/url"
 
 	"github.com/shanth1/hookrelay/internal/common"
 	"github.com/shanth1/hookrelay/internal/core/domain"
@@ -33,29 +31,10 @@ func (h *Handler) Handle(ctx context.Context, req ports.WebhookRequest) (*domain
 		return nil, common.ErrInvalidSignature
 	}
 
-	payloadJSON := ""
-	contentType := req.Headers["Content-Type"]
-
-	if contentType == "application/x-www-form-urlencoded" {
-		form, err := url.ParseQuery(string(req.Payload))
-		if err != nil {
-			return nil, fmt.Errorf("error parsing form payload: %w", err)
-		}
-		payloadJSON = form.Get("payload")
-	} else if contentType == "application/json" {
-		payloadJSON = string(req.Payload)
+	payload, eventName, err := parsePayload(req)
+	if err != nil {
+		return nil, common.ErrInvalidSignature
 	}
-
-	if payloadJSON == "" {
-		return nil, fmt.Errorf("payload is empty or content type is unsupported")
-	}
-
-	eventName := req.Headers["X-GitHub-Event"]
-	var payload map[string]interface{}
-	if err := json.Unmarshal([]byte(payloadJSON), &payload); err != nil {
-		return nil, fmt.Errorf("error parsing JSON payload: %w", err)
-	}
-	payload["eventName"] = eventName
 
 	templateName := common.GetTemplatePath(eventName)
 	if h.templates.Lookup(templateName) == nil {
